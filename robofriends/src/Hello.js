f0a4362b20e4f8958a8f975f2bd4edd83ee6e019
vmose <vicmose.vm@gmail.com>
2023-03-30 19:05:00 +0300

    Create Hello.js

---
+++ robofriends/src/Hello.js
@@ -0,0 +1,14 @@
+import React,{ Component } from "react";
+import './Hello.css';
+
+class Hello extends Component{
+    render(){
+        return (
+        <div>
+         <h1>Hello World</h1>
+         <p>What it do</p>
+         </div>)
+    }
+}
+
+export default Hello
