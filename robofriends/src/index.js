86f6e591f67876b4085aeb2c4c6901f50bf30746
vmose <vicmose.vm@gmail.com>
2023-03-30 20:49:20 +0300

    delete_unwanted

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Hello from './Hello';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Hello greeting = {'What ' + 'it do'}/>
+    <App greeting = {'What ' + 'it do'}/>
   </React.StrictMode>
 );
 
