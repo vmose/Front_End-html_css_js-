4146f54c9df50d412b684e22436f4150fe436cb7
vmose <vicmose.vm@gmail.com>
2023-03-30 21:05:51 +0300

    Create Card.js

---
+++ robofriends/src/Card.js
@@ -0,0 +1,16 @@
+// import React,{Component} from "react";
+import React from "react";
+// class Card extends Component{
+//     render()
+// }
+
+const Card = () =>{
+return (
+    <div>
+    <img alt='robot' src= "https://robohash.org/waturi"  />
+    <h1>Waturi</h1>
+    <p>waturi@mine.com</p>
+    </div>
+)}
+
+export default Card
