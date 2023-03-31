e2fcda1846b63a4c82ab9dda05d955a8675e88ce
vmose <vicmose.vm@gmail.com>
2023-03-31 21:31:01 +0300

    Update Card.js

--- robofriends/src/Card.js
+++ robofriends/src/Card.js
@@ -4,13 +4,24 @@ import React from "react";
 //     render()
 // }
 
-const Card = () =>{
+const Card = (props) =>{
 return (
-    <div className = 'bg-light-blue br3 dib pa3 ma2 grow bw2 shadow-5'>
-    <img alt='robot' src= "https://robohash.org/waturi?200x200"  />
-    <h1>Waturi</h1>
-    <p>waturi@mine.com</p>
+    <div className = 'tc bg-light-blue br3 dib pa3 ma2 grow bw2 shadow-5'>
+    <img alt='robot' src= {`https://robohash.org/${props.id}?200x200` } />
+    <h1>{props.name}</h1>
+    <p>{props.email}</p>
     </div>
 )}
+
+// alternative code
+// const Card = ({name,email,id}) =>{
+//     return (
+//         <div className = 'tc bg-light-blue br3 dib pa3 ma2 grow bw2 shadow-5'>
+//         <img alt='robot' src= {`https://robohash.org/${id}?200x200` } />
+//         <h1>{name}</h1>
+//         <p>{email}</p>
+//         </div>     
+//     )}
+    
 
 export default Card
