5b93d25ece6ee55d18fba860b2a442c2c50a2e40
vmose <vicmose.vm@gmail.com>
2023-04-01 23:48:21 +0300

    Update index.js

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -4,6 +4,7 @@ import './index.css'
 import reportWebVitals from './reportWebVitals'
 import 'tachyons'
 import App from './App'
+import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
