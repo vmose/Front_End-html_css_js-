31117b522366fa2085387c8d2fe8fe2590c06b40
vmose <vicmose.vm@gmail.com>
2023-04-01 23:48:09 +0300

    Update App.js

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -1,22 +1,33 @@
-import React, { Component } from "react";
-import Cardlist from "./CardList";
+import React, { Component } from "react"
+import Cardlist from "./CardList"
 import Searchbox from './Searchbox'
-import { robots } from "./robots";
+import { robots } from "./robots"
+import './App.css'
+
 
 class App extends Component {
 constructor(){
     super()
     this.state = {
         robots:robots,
-    searchstate: ""
+    searchfield: ""
     }
-
 }
+
+onSearchChange = (event) =>{
+    this.setState({searchfield: event.target.value})
+    console.log(event.target.value)
+}
+
 render(){
-    return  (<div>
-        <p>Robofriends</p>
-        <Searchbox/>
-        <Cardlist robot={robots}/>  
+    const filteredrobots = this.state.robots.filter(robots=>{
+        return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+    })
+    return  (<div className="tc">
+        <h1>Robofriends©</h1>
+        <p >by Victor Mose</p>
+        <Searchbox searchchange ={this.onSearchChange} />
+        <Cardlist robots={filteredrobots}/>  
         </div>
         )
 }
