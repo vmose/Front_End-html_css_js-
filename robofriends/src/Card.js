afe1046190714e84c5463b9600bc13915c83ecb2
vmose <vicmose.vm@gmail.com>
2023-04-01 23:48:13 +0300

    Update Card.js

--- robofriends/src/Card.js
+++ robofriends/src/Card.js
@@ -6,9 +6,9 @@ import React from "react";
 
 const Card = ({name,id,email}) =>{
 return (
-    <div className = 'tc bg-light-blue br3 dib pa3 ma2 grow bw2 shadow-5'>
+    <div className = 'tc bg-light-green br3 dib pa3 ma2 grow bw2 shadow-5'>
     <img alt='robot' src= {`https://robohash.org/${id}?200x200` } />
-    <h1>{name}</h1>
+    <h2>{name}</h2>
     <p>{email}</p>
     </div>
 )}
