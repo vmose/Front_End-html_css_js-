a2b51ec623f15d20f654d937f2ba38c5f185e643
vmose <vicmose.vm@gmail.com>
2023-04-03 18:20:01 +0300

    Create Scroll.js

---
+++ robofriends/src/Scroll.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Scroll = (props) =>{
+    <div> style={{overflowY:'scroll', border:'5px solid black', height:'800px'}}{props.children}</div>
+}
+
+export default Scroll
