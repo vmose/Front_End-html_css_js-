f28a0f2ede4ab32d4a27fba0d6028b6efb887bfb
vmose <vicmose.vm@gmail.com>
2023-08-30 23:31:05 +0300

    Create script.js

---
+++ HOSTINGER/script.js
@@ -0,0 +1,21 @@
+function updateCountdown() {
+    const now = new Date();
+    const targetDate = new Date("2023-12-31"); // Set your target date here
+    const timeLeft = targetDate - now;
+  
+    const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+  
+    document.getElementById("days").textContent = days.toString().padStart(2, "0");
+    document.getElementById("hours").textContent = hours.toString().padStart(2, "0");
+    document.getElementById("minutes").textContent = minutes.toString().padStart(2, "0");
+    document.getElementById("seconds").textContent = seconds.toString().padStart(2, "0");
+  }
+  
+//padStart(2, "0") ensures that each value is at least two digits long, adding a leading zero if necessary.
+
+  updateCountdown();
+  setInterval(updateCountdown, 1000);
+  
