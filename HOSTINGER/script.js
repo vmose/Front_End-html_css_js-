dfe81be9e6e006dd03687504d235dbce79041018
vmose <vicmose.vm@gmail.com>
2023-12-30 23:05:50 +0300

    Optimize script.js

--- HOSTINGER/script.js
+++ HOSTINGER/script.js
@@ -1,21 +1,20 @@
 function updateCountdown() {
-    const now = new Date();
-    const targetDate = new Date("2023-12-31"); // Set your target date here
-    const timeLeft = targetDate - now;
-  
-    const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
-  
-    document.getElementById("days").textContent = days.toString().padStart(2, "0");
-    document.getElementById("hours").textContent = hours.toString().padStart(2, "0");
-    document.getElementById("minutes").textContent = minutes.toString().padStart(2, "0");
-    document.getElementById("seconds").textContent = seconds.toString().padStart(2, "0");
-  }
-  
-//padStart(2, "0") ensures that each value is at least two digits long, adding a leading zero if necessary.
+  const targetDate = new Date("2023-12-31"); // Set your target date here
+  const now = new Date();
+  const timeLeft = targetDate - now;
 
-  updateCountdown();
-  setInterval(updateCountdown, 1000);
-  
+  const padWithZero = (value) => value.toString().padStart(2, "0");
+
+  const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+  document.getElementById("days").textContent = padWithZero(days);
+  document.getElementById("hours").textContent = padWithZero(hours);
+  document.getElementById("minutes").textContent = padWithZero(minutes);
+  document.getElementById("seconds").textContent = padWithZero(seconds);
+}
+
+updateCountdown();
+setInterval(updateCountdown, 1000);
