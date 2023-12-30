2bbd5d73afefd98469adab0c171e27e868958845
vmose <vicmose.vm@gmail.com>
2023-12-30 23:08:13 +0300

    Optimize Animated_Traffic_Lights/script.js

--- Animated_Traffic_Lights/script.js
+++ Animated_Traffic_Lights/script.js
@@ -1,12 +1,11 @@
 const circles = document.querySelectorAll('.circle');
-        let activeLight = 0;
-        setInterval(changeLight, 1000);
-        function changeLight() {
-            circles[activeLight].className = 'circle';
-            activeLight++;
-            if (activeLight > 2) {
-                activeLight = 0;
-            }
-            const currentLight = circles[activeLight];
-            currentLight.classList.add(currentLight.getAttribute('color'));
-        }
+let activeLight = 0;
+
+function changeLight() {
+    circles[activeLight].classList.remove(circles[activeLight].getAttribute('color'));
+    activeLight = (activeLight + 1) % circles.length;
+    const currentLight = circles[activeLight];
+    currentLight.classList.add(currentLight.getAttribute('color'));
+}
+
+setInterval(changeLight, 1000);
